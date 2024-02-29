7957a7c8f42d44863502577d1f3d1244e063fe97
Vinselcia <153624107+Vinselcia@users.noreply.github.com>
2024-02-29 18:12:52 +0530

    Details

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeDetails.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeDetails.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        static List<Employee> listEmployee = new List<Employee>()
+        { 
+            new Employee(){Email="rutuja@gmail.com",Name="Rutuja"},
+             new Employee(){Email="mary@gmail.com",Name="Mary",},
+              new Employee(){Email="sylvia@gmail.com",Name="Sylvia"},
+               new Employee(){Email="jebaa@gmail.com",Name="Jeba"}
+        };
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
